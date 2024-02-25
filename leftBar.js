b12d0bb579d28f230e592d55e63f5e329c225cd4
codershub-ing <154464457+codershub-ing@users.noreply.github.com>
2024-02-25 19:18:33 +0000

    comittingChangesThatAddHtmlWithJS

---
+++ leftBar.js
@@ -0,0 +1,47 @@
+
+    let leftBar = document.querySelector(".leftBar");
+    leftBar.innerHTML =leftBar.innerHTML + `
+   
+<div class="leftBarIcoDiv">
+   <a>
+       <img class="scM1" src="https://mymusic-dev.freewebhostmost.com/icons/info.png" alt="">
+   </a>
+
+</div>
+<div class="leftBarIcoDiv">
+   <a href="https://www.instagram.com/codershub.ing?igsh=cDNmZ3U5NHYyY2Rl" target="_blank">
+       <img class="scM2" src="https://mymusic-dev.freewebhostmost.com/icons/instagram.png" alt="">
+       <p>Instagram</p>
+   </a>
+
+</div>
+<div class="leftBarIcoDiv">
+   <a href="" target="_blank"> <img class="scM3"
+           src="https://mymusic-dev.freewebhostmost.com/icons/gitHub.png" alt="">
+       <p>GitHub</p>
+   </a>
+
+</div>
+<div class="leftBarIcoDiv">
+   <a href="" target="_blank"> <img class="scM4"
+           src="https://mymusic-dev.freewebhostmost.com/icons/reddit.png" alt="">
+       <p>Reddit</p>
+   </a>
+
+
+</div>
+<div class="leftBarIcoDiv">
+   <a href="" target="_blank"><img class="scM5"
+           src="https://mymusic-dev.freewebhostmost.com/icons/email.png" alt="">
+       <p>Email</p>
+   </a>
+
+
+</div>
+<div class="leftBarIcoDiv">
+   <a href="" target="_blank"><img class="scM6"
+           src="https://mymusic-dev.freewebhostmost.com/icons/youTube.png" alt="">
+       <p>YouTube</p>
+   </a>
+
+</div>`
---
+++ leftMenu.js
@@ -0,0 +1,16 @@
+document.querySelector(".leftMenu").innerHTML =document.querySelector(".leftMenu").innerHTML +  `  
+ <div class="leftMenuDiv">
+<a href="https://mymusic-dev.freewebhostmost.com/rb/index.html">rb</a>
+</div>
+<div class="leftMenuDiv">
+<a href="https://mymusic-dev.freewebhostmost.com/new/index.html">JS property</a>
+</div>
+<div class="leftMenuDiv">
+<a href="https://mymusic-dev.freewebhostmost.com/n-factorial/index.html">math</a>
+</div>
+<div class="leftMenuDiv">
+<a href="https://mymusic-dev.freewebhostmost.com/FindPercentage/index.html">Find Percentage</a>
+</div>
+<div class="leftMenuDiv">
+<a href="https://mymusic-dev.freewebhostmost.com/main/index.html">Main</a>
+</div>`
